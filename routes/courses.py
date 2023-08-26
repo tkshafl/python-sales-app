2351467b6ac2615cf048aa3b7ad2d28bc666175b
Tushar Sha <tusharksha@gmail.com>
2023-08-26 16:30:31 -0400

    added course detail

--- routes/courses.py
+++ routes/courses.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from app import app
 from app import db
 from models.courses import Course
@@ -8,3 +8,9 @@ def courses():
     course_recs = db.session.query(Course).all() 
     courses = list(map(lambda rec: rec.__dict__, course_recs))
     return render_template('courses.html', courses = courses)
+
+@app.route('/course',methods=['get'])
+def course():
+    id = request.args.get('id')
+    course = Course.query.get(id)     
+    return render_template('course_detail.html', course = course)
