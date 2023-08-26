b2b916521fd220939b6c1d010bae195b0807f319
Tushar Sha <tusharksha@gmail.com>
2023-08-26 15:46:02 -0400

    added user detail

--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from app import app
 from app import db
 from models.users import User
@@ -8,3 +8,14 @@ def users():
     user_recs = db.session.query(User).all()
     users = list(map(lambda rec: rec.__dict__, user_recs))
     return render_template('users.html', users = users)
+
+# @app.route('/user/<int:id>',methods=['get'])
+# def user(id):
+#     user = User.query.get(id)     
+#     return render_template('user_detail.html', user = users)
+
+@app.route('/user',methods=['get'])
+def user():
+    id = request.args.get('id')
+    user = User.query.get(id)     
+    return render_template('user_detail.html', user = user)
